20f743cae9d6937ab41b9ae6a9282a51354058f1
Robert Kety <robertkety@gmail.com>
2014-11-27 00:41:34 -0800

    Added notes
    I added a bunch of notes. We'll probably have to document something
    later. Better to start recording our thoughts now. Also, I'm waiting on
    a response regarding our approach to partitioning. Until then, I've just
    added notes

--- src/UrlValidatorTest.java
+++ src/UrlValidatorTest.java
@@ -40,6 +40,23 @@ public class UrlValidatorTest extends TestCase {
    
    public void testManualTest()
    {
+	   /*
+	    * My method here does not follow a proper test plan.  We didn't appear to focus on test plans, which are a 
+	    * priority for proper testing. A good test plan would steer manual testing in a proper direction. What we
+	    * have here is just one persons attempt to out-think the implementation.  I researched a lot of the w3 
+	    * standards to produce my tests, but all of this simply adds up to aiding our partitioning effort in part
+	    * two.
+	    * If you'd like to visit some of my research links:
+	    * http://en.wikipedia.org/wiki/Uniform_resource_locator
+	    * http://en.wikipedia.org/wiki/List_of_Internet_top-level_domains
+	    * http://en.wikipedia.org/wiki/URI_scheme
+	    * http://www.w3.org/Addressing/URL/url-spec.html
+	    * http://www.w3.org/Addressing/URL/uri-spec.html	//Not a duplicate, this focuses on urI not urL
+	    * http://www.boutell.com/newfaq/creating/domainlength.html
+	    * http://www.dwheeler.com/essays/debugging-agans.html //Not related here, but we'll need it to answer one of the later questions
+	    * 
+	    */
+	   
 	   String thisString = "";
 	   UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
 
@@ -49,8 +66,9 @@ public class UrlValidatorTest extends TestCase {
 	   System.out.println(String.format("%-70s", thisString = "http://amazon.com") + urlVal.isValid(thisString));
 	   System.out.println(String.format("%-70s", thisString = "www.hero6.org") + urlVal.isValid(thisString));
 	   System.out.println(String.format("%-70s", thisString = "hero6.org") + urlVal.isValid(thisString));
-	   System.out.println(String.format("%-70s", thisString = "24.20.29.121") + urlVal.isValid(thisString));
-	   System.out.println(String.format("%-70s", thisString = "http://[2a00:1450:4007:80b::1008]/") + urlVal.isValid(thisString));
+	   System.out.println(String.format("%-70s", thisString = "http://74.125.224.72/") + urlVal.isValid(thisString));
+	   System.out.println(String.format("%-70s", thisString = "74.125.224.72") + urlVal.isValid(thisString));
+	   System.out.println(String.format("%-70s", thisString = "http://[2a00:1450:8003::93]/") + urlVal.isValid(thisString));
 	   System.out.println(String.format("%-70s", thisString = "ftp://google.com") + urlVal.isValid(thisString));
 	   System.out.println(String.format("%-70s", thisString = "bolo://bolo.com") + urlVal.isValid(thisString));
 	   System.out.println(String.format("%-70s", thisString = "bitcoin://google.com") + urlVal.isValid(thisString));
@@ -112,7 +130,45 @@ public class UrlValidatorTest extends TestCase {
    
    public void testYourFirstPartition()
    {
-	   
+	   /* 
+	    * Partitioning ideas:
+	    * ipv4
+	    * ipv6
+	    * url:  - The primary partition, we can probably lump together the ipvX tests into a separate partition
+	    * 	According to http://en.wikipedia.org/wiki/Uniform_resource_locator the full url format is:
+	    * 		scheme://domain:port/path?query_string#fragment_id
+	    * 	However, I have seen alternative schemes at http://www.w3.org/Addressing/URL/url-spec.html#z49
+	    * 
+	    * 	I say we break this into simple, extended, and advanced partitions
+	    * 	Simple: 
+	    * 		scheme://domain/path
+	    * 	Extended:
+	    * 		scheme://domain:port/path?query_string#fragment_id
+	    * 	Advanced:
+	    * 		IPV4:
+	    * 			scheme://255.255.255.255:port/path?query_string#fragment_id
+	    * 		IPV6:
+	    *			scheme://[2a00:1450:8003::93]:port/path?query_string#fragment_id
+	    *		mailto:emailaddress@hostname			(Email)
+	    *		ftp://username:password@domain			(Login credentials)
+	    *		http://xn--sterreich-z7a.icom.museum/	(Internationalized web addresses)
+	    * 
+	    * 	Each of these partitions will overlap with the follow universal domain boundaries:
+	    * 		Unreserved characters:	http://en.wikipedia.org/wiki/Uniform_resource_locator#Unreserved
+	    * 		Reserved characters: http://en.wikipedia.org/wiki/Uniform_resource_locator#Reserved  //This will be negative cases unless we encode them (see http://en.wikipedia.org/wiki/Percent-encoding)
+	    * 		Minimum and maximum field limits: I found one mention of maximum at:  http://www.boutell.com/newfaq/creating/domainlength.html
+	    * 		Domain name variations: There are many variations around. Most of us are familiar with TLDs (See http://en.wikipedia.org/wiki/List_of_Internet_top-level_domains) 
+	    * 			For example: 
+	    * 				www.state.or.us
+	    * 				http://www.vmr.gov.ua/
+	    * 		Null/Empty-string Permutations: We need to test when certain components are no added (e.g., missing path, missing scheme, etc)
+	    * 			For example:
+	    * 				http://google.com	(No subdomain, no path)
+	    * 				sftp://192.168.1.1 	(No port, no path)
+	    * 				www.google.com/path	(No scheme)
+	    * 				
+	    */		
+	    			
    }
    
    public void testYourSecondPartition(){
